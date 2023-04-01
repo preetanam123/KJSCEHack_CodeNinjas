775a01f1bdb7567757045ca7c23940673d205425
preetanam123 <80145584+preetanam123@users.noreply.github.com>
2023-04-02 01:47:40 +0530

    ocr

--- flask/ocr4.py
+++ flask/ocr4.py
@@ -1,17 +1,17 @@
-from pdf2image import convert_from_path
+# from pdf2image import convert_from_path
 import cv2
 from PIL import Image
 import pytesseract
 pytesseract.pytesseract.tesseract_cmd = r'C:\Program Files\Tesseract-OCR\tesseract.exe'
 
-pdfs = r"report.pdf"
-pages = convert_from_path(pdfs, 350)
+# pdfs = r"report.pdf"
+# pages = convert_from_path(pdfs, 350)
 
-i = 1
-for page in pages:
-    image_name = "Page_" + str(i) + ".jpg"  
-    page.save(image_name, "JPEG")
-    i = i+1  
+# i = 1
+# for page in pages:
+#     image_name = "Page_" + str(i) + ".jpg"  
+#     page.save(image_name, "JPEG")
+#     i = i+1  
 
 
 ans = pytesseract.image_to_string(Image.open('Page_1.jpg'))
